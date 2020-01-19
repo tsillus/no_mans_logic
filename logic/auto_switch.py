d2c2fd0f5c65b2e97ab48ce2900b512f4961b1a2
tekay37 <torstensillus@gmail.com>
2020-01-19 22:13:01 +0100

    refactored AutoSwitch to a subclass of Gate

--- logic/auto_switch.py
+++ logic/auto_switch.py
@@ -1,18 +1,10 @@
+from logic.gate import Gate
 from logic.signal import Signal
-from logic.wire import Wire
-from model.vector import Vector
 
 
-class AutoSwitch(object):
-    def __init__(self, position: Vector, direction: Vector, is_open=False):
-        self.direction = direction
-        self.position = position
-        self.gate = Wire(position + direction.rotate(3), position + direction.rotate(1))
-        self.is_open = is_open
+class AutoSwitch(Gate):
 
     def tick(self, signals):
-        output = []
-        if self.is_open:
-            output = self.gate.tick(signals)
+        output = super(AutoSwitch, self).tick(signals)
         self.is_open = Signal(True, self.position + self.direction) in signals
         return output
